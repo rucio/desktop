ef539d3ac1174914a7fb7b22ab0af3eb719963f6
Vivek Nigam <viveknigam.nigam3@gmail.com>
2020-08-12 21:38:51 +0530

    Added more RSE APIs and Routes

--- server/APIs/rse.js
+++ server/APIs/rse.js
@@ -22,4 +22,112 @@ async function getRSEs(certlocation, server, token) {
     .then(console.log(`[INFO] RSE list retrieved for ${server.name}`));
 }
 
-module.exports = { getRSEs };
+/**
+ * Gets settings and protocols of an `rse` for a Rucio Server.
+ * @param {String} certlocation
+ * @param {{name: String, host: String, auth: String}} server
+ * @param {String} token
+ * @param {String} rse Valid Rucio Storage Element (RSE) Name
+ */
+async function info(certlocation, server, token, rse) {
+  const httpsAgent = new https.Agent({ ca: fs.readFileSync(certlocation) });
+  return axios
+    .get(`https://${server.host}/rses/${rse}`, {
+      httpsAgent,
+      headers: {
+        "User-Agent": `rucio-desktop/${packageJSON.version}`,
+        "X-Rucio-Auth-Token": token,
+      },
+    })
+    .then(console.log(`[INFO] Info retreived for ${rse} from ${server.name}`));
+}
+
+/**
+ * Gets attributes of an `rse` for a Rucio Server.
+ * @param {String} certlocation
+ * @param {{name: String, host: String, auth: String}} server
+ * @param {String} token
+ * @param {String} rse Valid Rucio Storage Element (RSE) Name
+ */
+async function attributes(certlocation, server, token, rse) {
+  const httpsAgent = new https.Agent({ ca: fs.readFileSync(certlocation) });
+  return axios
+    .get(`https://${server.host}/rses/${rse}/attr/`, {
+      httpsAgent,
+      headers: {
+        "User-Agent": `rucio-desktop/${packageJSON.version}`,
+        "X-Rucio-Auth-Token": token,
+      },
+    })
+    .then(
+      console.log(`[INFO] Attributes retreived for ${rse} from ${server.name}`)
+    );
+}
+
+/**
+ * Gets only protocols of an `rse` for a Rucio Server.
+ * @param {String} certlocation
+ * @param {{name: String, host: String, auth: String}} server
+ * @param {String} token
+ * @param {String} rse Valid Rucio Storage Element (RSE) Name
+ */
+async function protocols(certlocation, server, token, rse) {
+  const httpsAgent = new https.Agent({ ca: fs.readFileSync(certlocation) });
+  return axios
+    .get(`https://${server.host}/rses/${rse}/protocols`, {
+      httpsAgent,
+      headers: {
+        "User-Agent": `rucio-desktop/${packageJSON.version}`,
+        "X-Rucio-Auth-Token": token,
+      },
+    })
+    .then(
+      console.log(`[INFO] Protocols retreived for ${rse} from ${server.name}`)
+    );
+}
+
+/**
+ * Gets usage of an `rse` for a Rucio Server.
+ * @param {String} certlocation
+ * @param {{name: String, host: String, auth: String}} server
+ * @param {String} token
+ * @param {String} rse Valid Rucio Storage Element (RSE) Name
+ */
+async function usage(certlocation, server, token, rse) {
+  const httpsAgent = new https.Agent({ ca: fs.readFileSync(certlocation) });
+  return axios
+    .get(`https://${server.host}/rses/${rse}/usage`, {
+      httpsAgent,
+      headers: {
+        "User-Agent": `rucio-desktop/${packageJSON.version}`,
+        "X-Rucio-Auth-Token": token,
+      },
+    })
+    .then(
+      console.log(`[INFO] RSE Usage retreived for ${rse} from ${server.name}`)
+    );
+}
+
+/**
+ * Gets limits of an `rse` for a Rucio Server.
+ * @param {String} certlocation
+ * @param {{name: String, host: String, auth: String}} server
+ * @param {String} token
+ * @param {String} rse Valid Rucio Storage Element (RSE) Name
+ */
+async function limits(certlocation, server, token, rse) {
+  const httpsAgent = new https.Agent({ ca: fs.readFileSync(certlocation) });
+  return axios
+    .get(`https://${server.host}/rses/${rse}/limits`, {
+      httpsAgent,
+      headers: {
+        "User-Agent": `rucio-desktop/${packageJSON.version}`,
+        "X-Rucio-Auth-Token": token,
+      },
+    })
+    .then(
+      console.log(`[INFO] RSE Limits retreived for ${rse} from ${server.name}`)
+    );
+}
+
+module.exports = { getRSEs, info, attributes, protocols, usage, limits };
--- server/routes/rse.js
+++ server/routes/rse.js
@@ -13,4 +13,84 @@ router.post("/rses", async (req, res) => {
     });
 });
 
+router.post("/rse/info", async (req, res) => {
+  const payload = req.body.payload;
+
+  await RSE.info(
+    payload.certlocation,
+    payload.server,
+    payload.token,
+    payload.rse
+  )
+    .then((info) => res.send(info.data))
+    .catch((err) => {
+      console.log(`[ERROR: /rse/info] ${err}`);
+      res.sendStatus(401);
+    });
+});
+
+router.post("/rse/attr", async (req, res) => {
+  const payload = req.body.payload;
+
+  await RSE.attributes(
+    payload.certlocation,
+    payload.server,
+    payload.token,
+    payload.rse
+  )
+    .then((attr) => res.send(attr.data))
+    .catch((err) => {
+      console.log(`[ERROR: /rse/attr] ${err}`);
+      res.sendStatus(err.response.status);
+    });
+});
+
+router.post("/rse/protocols", async (req, res) => {
+  const payload = req.body.payload;
+
+  await RSE.protocols(
+    payload.certlocation,
+    payload.server,
+    payload.token,
+    payload.rse
+  )
+    .then((protocols) => res.send(protocols.data))
+    .catch((err) => {
+      console.log(`[ERROR: /rse/protocols] ${err}`);
+      res.sendStatus(err.response.status);
+    });
+});
+
+router.post("/rse/usage", async (req, res) => {
+  const payload = req.body.payload;
+
+  await RSE.usage(
+    payload.certlocation,
+    payload.server,
+    payload.token,
+    payload.rse
+  )
+    .then((usage) => res.send(usage.data))
+    .catch((err) => {
+      console.log(`[ERROR: /rse/usage] ${err}`);
+      res.sendStatus(err.response.status);
+    });
+});
+
+router.post("/rse/limits", async (req, res) => {
+  const payload = req.body.payload;
+
+  await RSE.limits(
+    payload.certlocation,
+    payload.server,
+    payload.token,
+    payload.rse
+  )
+    .then((limits) => res.send(limits.data))
+    .catch((err) => {
+      console.log(`[ERROR: /rse/limits] ${err}`);
+      res.sendStatus(err.response.status);
+    });
+});
+
 module.exports = router;
