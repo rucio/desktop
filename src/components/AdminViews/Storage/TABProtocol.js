4d27f0203504bcc6c5d222cd72f5a2eb09423369
Vivek Nigam <viveknigam.nigam3@gmail.com>
2020-08-18 19:55:04 +0530

    Updated RSE Info Tabs

--- src/components/AdminViews/Storage/TABProtocol.js
+++ src/components/AdminViews/Storage/TABProtocol.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import TabPanel from "./RSETabPanel";
 import { TextField, makeStyles, InputLabel } from "@material-ui/core";
+import ColoredLine from "../../Utils/ColoredLine";
 
 const useStyles = makeStyles({
   inputLabel: {
@@ -19,6 +20,23 @@ const useStyles = makeStyles({
 
 function TABProtocol(props) {
   const classes = useStyles();
+  // const [values, setValues] = React.useState({
+  //   extended_attributes: props.protocols.extended_attributes,
+  //     hostname: props.protocols.hostname,
+  //     prefix: props.protocols.prefix,
+  //     domains: props.protocols.domains,
+  //     scheme: props.protocols.scheme,
+  //     port: props.protocols.port,
+  //     impl: props.protocols.impl,
+  // });
+
+  // const handleChange = (event) => {
+  //   setValues({
+  //     ...values,
+  //     [event.target.name]: event.target.value,
+  //   });
+  // };
+  
   return (
     <TabPanel value={props.value} index={1}>
       {props.protocols.map((item) => (
@@ -30,6 +48,7 @@ function TABProtocol(props) {
                 variant="outlined"
                 size="small"
                 fullWidth
+                name="hostname"
                 defaultValue={item.hostname}
               />
             </div>
@@ -39,6 +58,7 @@ function TABProtocol(props) {
                 variant="outlined"
                 size="small"
                 type="number"
+                name="port"
                 className={classes.textfield}
                 defaultValue={item.port}
               />
@@ -48,10 +68,27 @@ function TABProtocol(props) {
           <TextField
             variant="outlined"
             size="small"
+            name="item"
             className={classes.textfield}
             defaultValue={item.prefix}
           />
           <InputLabel className={classes.inputLabel}>Scheme</InputLabel>
+          <TextField
+            variant="outlined"
+            size="small"
+            name="scheme"
+            className={classes.textfield}
+            defaultValue={item.scheme}
+          />
+           <InputLabel className={classes.inputLabel}>Protocol</InputLabel>
+           <TextField
+            variant="outlined"
+            size="small"
+            name="impl"
+            className={classes.textfield}
+            defaultValue={item.impl}
+          />
+          <ColoredLine color="#000000" opacity={0.2}/>
         </div>
       ))}
     </TabPanel>
--- src/components/AdminViews/Storage/TabGeneral.js
+++ src/components/AdminViews/Storage/TabGeneral.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import TabPanel from "./RSETabPanel";
-import { makeStyles, Checkbox } from "@material-ui/core";
+import { makeStyles, Checkbox, InputLabel, TextField } from "@material-ui/core";
 
 const useStyles = makeStyles({
   preInfo: {
@@ -17,46 +17,98 @@ const useStyles = makeStyles({
     alignItems: "center",
     marginBottom: "1rem",
   },
+  textfield: {
+    width: "50%",
+    marginBottom: 20,
+  },
+  inputLabel: {
+    marginBottom: 10,
+    color: "#354992",
+  },
 });
 
 function TabGeneral(props) {
-  const processRegion = () => {
-    const city = props.moreDetails.city ? props.moreDetails.city : "";
-    const country = props.moreDetails.country_name
-      ? props.moreDetails.country_name
-      : "";
-    const continent = props.moreDetails.continent
-      ? props.moreDetails.continent
-      : "";
-    const regionCode = props.moreDetails.region_code
-      ? `(${props.moreDetails.region_code})`
-      : "";
-    const regionStr = `${city} ${country} ${continent} ${regionCode}`;
+  const [values, setValues] = React.useState({});
 
-    return regionStr.length > 3 ? regionStr : "Unknown";
+  function isPresent(x) {
+    return x || "";
+  }
+
+  const handleChange = (event) => {
+    setValues({
+      ...values,
+      [event.target.name]: event.target.value,
+    });
   };
 
   const classes = useStyles();
   return (
     <TabPanel value={props.value} index={0}>
-      <div id="type" className={classes.info}>
-        <span className={classes.preInfo}>Type: </span>
-        {props.details.rse_type}
+      <InputLabel className={classes.inputLabel}>Type</InputLabel>
+      <TextField
+        variant="outlined"
+        size="small"
+        name="rse_type"
+        className={classes.textfield}
+        value={props.details.rse_type}
+        onChange={handleChange}
+      />
+      <InputLabel className={classes.inputLabel}>Region</InputLabel>
+      <div style={{ display: "flex", flex: 1 }}>
+        <TextField
+          variant="outlined"
+          size="small"
+          name="city"
+          placeholder="City"
+          className={classes.textfield}
+          value={isPresent(props.moreDetails.city)}
+          onChange={handleChange}
+        />
+        <TextField
+          variant="outlined"
+          size="small"
+          name="country_name"
+          placeholder="Country Name"
+          className={classes.textfield}
+          value={isPresent(props.moreDetails.country_name)}
+          onChange={handleChange}
+        />
+        <TextField
+          variant="outlined"
+          size="small"
+          name="continent"
+          placeholder="Continent"
+          className={classes.textfield}
+          value={isPresent(props.moreDetails.continent)}
+          onChange={handleChange}
+        />
+        <TextField
+          variant="outlined"
+          size="small"
+          name="region_code"
+          type="number"
+          placeholder="Region Code"
+          className={classes.textfield}
+          value={isPresent(props.moreDetails.region_code)}
+          onChange={handleChange}
+        />
       </div>
-      <div id="volatile" className={classes.info}>
-        <span className={classes.preInfo}>Region: </span>
-        {processRegion()}
-      </div>
-      <div id="lfn2pfn" className={classes.info}>
-        <span className={classes.preInfo}>LFN2PFN Algorithm: </span>
-        {props.details.lfn2pfn_algorithm}
-      </div>
+      <InputLabel className={classes.inputLabel}>LFN2PFN Algorithm</InputLabel>
+      <TextField
+        variant="outlined"
+        size="small"
+        name="lfn2pfn_algorithm"
+        className={classes.textfield}
+        value={isPresent(props.details.lfn2pfn_algorithm)}
+        onChange={handleChange}
+      />
       <div id="deterministic" className={classes.info}>
-        <span className={classes.preInfo}>Deterministic: </span>
+        <InputLabel className={classes.inputLabel}>Deterministic</InputLabel>
         <Checkbox
           checked={props.details.deterministic}
-          name="present"
+          name="deterministic"
           color="primary"
+          onChange={(e) => handleChange(e)}
         />
       </div>
       <div id="volatile" className={classes.info}>
