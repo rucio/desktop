d785d7663bc5da2127f1dc1d32a5e69bda00ba55
Vivek Nigam <viveknigam.nigam3@gmail.com>
2020-08-20 11:29:42 +0530

    Displaying Changes for Review

---
+++ src/components/AdminViews/Storage/ConfirmChangeDialog.js
@@ -0,0 +1,107 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { useSelector } from "react-redux";
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  makeStyles,
+  DialogActions,
+  Button,
+} from "@material-ui/core";
+import LoadingSpinner from "../../Utils/LoadingSpinner";
+import { green, red } from "@material-ui/core/colors";
+
+const useStyles = makeStyles({
+  root: {
+    fontFamily: "Cern",
+  },
+  title: {
+    fontFamily: "Cern",
+    margin: "0.6rem",
+  },
+  message: {
+    fontSize: "1rem",
+    fontWeight: 500,
+    opacity: 0.8,
+    paddingTop: 0,
+    padding: "0.6rem",
+    lineHeight: 1.5,
+  },
+  changelog: {
+    color: "#000000",
+    opacity: 0.8,
+    padding: "0.6rem",
+    maxHeight: "16rem",
+    overflow: "auto"
+  },
+  existing: {
+    backgroundColor: red[50],
+    padding: "0.4rem",
+  },
+  incoming: {
+    backgroundColor: green[50],
+    padding: "0.4rem",
+    marginBottom: "0.8rem",
+  },
+});
+
+function ConfirmChangeDialog(props) {
+  const classes = useStyles();
+  const changes = props.changes;
+  const loading = useSelector((state) => state.loading);
+
+  return (
+    <Dialog open={props.open} onClose={props.handleClose}>
+      <DialogTitle className={classes.title} id="confirm-title">
+        {"Review Changes"}
+      </DialogTitle>
+      <DialogContent>
+        {loading ? (
+          <LoadingSpinner message={`Updating RSE Parameters. Please Wait`} />
+        ) : (
+          <div id="changes-content" className={classes.root}>
+            <p id="message" className={classes.message}>
+              The following changes were made and the new settings will be
+              applied. Review your changes before confirming.
+            </p>
+            <div id="changelog" className={classes.changelog}>
+              {Object.keys(changes).map((key) => (
+                <React.Fragment key={key}>
+                  <div id="existing" className={classes.existing}>
+                    {" "}
+                    - {key}: {props.initialValues[props.currentIndex][key]}
+                  </div>
+                  <div id="incoming" className={classes.incoming}>
+                    {" "}
+                    + {key}: {changes[key]}
+                  </div>
+                </React.Fragment>
+              ))}
+            </div>
+          </div>
+        )}
+      </DialogContent>
+      <DialogActions>
+        <Button
+          color="secondary"
+          variant="outlined"
+          onClick={props.handleClose}
+        >
+          Cancel
+        </Button>
+        <Button color="primary">Confirm Changes</Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
+
+ConfirmChangeDialog.propTypes = {
+  open: PropTypes.bool,
+  handleClose: PropTypes.func,
+  changes: PropTypes.object,
+  initialValues: PropTypes.array,
+  currentIndex: PropTypes.number,
+};
+
+export default ConfirmChangeDialog;
--- src/components/AdminViews/Storage/TABProtocol.js
+++ src/components/AdminViews/Storage/TABProtocol.js
@@ -9,6 +9,7 @@ import {
   Button,
 } from "@material-ui/core";
 import ColoredLine from "../../Utils/ColoredLine";
+import ConfirmChangeDialog from "./ConfirmChangeDialog";
 
 const useStyles = makeStyles({
   inputLabel: {
@@ -30,22 +31,21 @@ const useStyles = makeStyles({
 
 function TABProtocol(props) {
   const classes = useStyles();
-  // eslint-disable-next-line no-unused-vars
   const initialProtocols = props.protocols;
   const [currentIndex, setCurrentIndex] = React.useState(null);
+  // eslint-disable-next-line no-unused-vars
   const [values, setValues] = React.useState(null);
+  const [changes, setChanges] = React.useState({});
   const [nextProps, setNextProps] = React.useState(props);
+  const [open, setOpen] = React.useState(false);
 
   React.useEffect(() => {
     setNextProps({});
     setNextProps(props);
     setValues(null);
     setCurrentIndex(null);
+    setChanges({})
   }, [props]);
-
-  React.useEffect(() => {
-    console.log(values)
-  }, [values])
 
   const handleChange = (event, index) => {
     setNextProps({
@@ -55,146 +55,160 @@ function TABProtocol(props) {
           ...nextProps.protocols[index],
           [event.target.name]: event.target.value,
         },
-        ...nextProps.protocols.slice(index+1),
+        ...nextProps.protocols.slice(index + 1),
       ],
     });
-    setCurrentIndex(index)
+    setCurrentIndex(index);
     setValues({
       ...nextProps.protocols[index],
+      [event.target.name]: event.target.value,
+    });
+    setChanges({
+      ...changes,
       [event.target.name]: event.target.value,
     });
   };
 
   return (
-    <TabPanel value={props.value} index={1}>
-      {nextProps.protocols.map((item, index) => (
-        <div key={`${item.hostname}:${item.port}`}>
-          <div style={{ display: "flex", width: "100%" }}>
-            <div>
-              <InputLabel className={classes.inputLabel}>Hostname</InputLabel>
-              <TextField
-                variant="outlined"
-                size="small"
-                fullWidth
-                name="hostname"
-                value={item.hostname}
-                onChange={(e) => handleChange(e, index)}
-              />
+    <React.Fragment>
+      <TabPanel value={props.value} index={1}>
+        {nextProps.protocols.map((item, index) => (
+          <div key={`${item.hostname}:${item.port}`}>
+            <div style={{ display: "flex", width: "100%" }}>
+              <div>
+                <InputLabel className={classes.inputLabel}>Hostname</InputLabel>
+                <TextField
+                  variant="outlined"
+                  size="small"
+                  fullWidth
+                  name="hostname"
+                  value={item.hostname}
+                  onChange={(e) => handleChange(e, index)}
+                />
+              </div>
+              <div>
+                <InputLabel className={classes.inputLabel}>Port</InputLabel>
+                <TextField
+                  variant="outlined"
+                  size="small"
+                  type="number"
+                  name="port"
+                  className={classes.textfield}
+                  value={item.port}
+                  onChange={(e) => handleChange(e, index)}
+                />
+              </div>
             </div>
-            <div>
-              <InputLabel className={classes.inputLabel}>Port</InputLabel>
+            <InputLabel className={classes.inputLabel}>Prefix</InputLabel>
+            <TextField
+              variant="outlined"
+              size="small"
+              name="prefix"
+              className={classes.textfield}
+              value={item.prefix}
+              onChange={(e) => handleChange(e, index)}
+            />
+            <InputLabel className={classes.inputLabel}>Scheme</InputLabel>
+            <TextField
+              variant="outlined"
+              size="small"
+              name="scheme"
+              className={classes.textfield}
+              value={item.scheme}
+              onChange={(e) => handleChange(e, index)}
+            />
+            <InputLabel className={classes.inputLabel}>Protocol</InputLabel>
+            <TextField
+              variant="outlined"
+              size="small"
+              name="impl"
+              className={classes.textfield}
+              value={item.impl}
+              onChange={(e) => handleChange(e, index)}
+            />
+            <InputLabel className={classes.inputLabel}>Domains</InputLabel>
+            <InputLabel className={classes.inputLabel}>LAN</InputLabel>
+            <FormGroup row style={{ paddingBottom: "1rem" }}>
               <TextField
-                variant="outlined"
+                className={classes.smallTextField}
                 size="small"
+                variant="outlined"
                 type="number"
-                name="port"
-                className={classes.textfield}
-                value={item.port}
-                onChange={(e) => handleChange(e, index)}
+                label="Read"
+                value={item.domains.lan.read}
               />
-            </div>
+              <TextField
+                className={classes.smallTextField}
+                size="small"
+                variant="outlined"
+                type="number"
+                label="Write"
+                value={item.domains.lan.write}
+              />
+              <TextField
+                className={classes.smallTextField}
+                size="small"
+                variant="outlined"
+                type="number"
+                label="Delete"
+                value={item.domains.lan.delete}
+              />
+            </FormGroup>
+            <InputLabel className={classes.inputLabel}>WAN</InputLabel>
+            <FormGroup row style={{ paddingBottom: "1rem" }}>
+              <TextField
+                className={classes.smallTextField}
+                size="small"
+                variant="outlined"
+                type="number"
+                label="Read"
+                value={item.domains.wan.read}
+              />
+              <TextField
+                className={classes.smallTextField}
+                size="small"
+                variant="outlined"
+                type="number"
+                label="Write"
+                value={item.domains.wan.write}
+              />
+              <TextField
+                className={classes.smallTextField}
+                size="small"
+                variant="outlined"
+                type="number"
+                label="Delete"
+                value={item.domains.wan.delete}
+              />
+              <TextField
+                className={classes.smallTextField}
+                size="small"
+                variant="outlined"
+                type="number"
+                label="Third Party Copy"
+                value={item.domains.wan.third_party_copy}
+              />
+            </FormGroup>
+            <Button
+              variant="contained"
+              color="primary"
+              disabled={index !== currentIndex}
+              onClick={() => setOpen(true)}
+            >
+              Update Protocol
+            </Button>
+            <ColoredLine color="#000000" opacity={0.2} />
           </div>
-          <InputLabel className={classes.inputLabel}>Prefix</InputLabel>
-          <TextField
-            variant="outlined"
-            size="small"
-            name="prefix"
-            className={classes.textfield}
-            value={item.prefix}
-            onChange={(e) => handleChange(e, index)}
-          />
-          <InputLabel className={classes.inputLabel}>Scheme</InputLabel>
-          <TextField
-            variant="outlined"
-            size="small"
-            name="scheme"
-            className={classes.textfield}
-            value={item.scheme}
-            onChange={(e) => handleChange(e, index)}
-          />
-          <InputLabel className={classes.inputLabel}>Protocol</InputLabel>
-          <TextField
-            variant="outlined"
-            size="small"
-            name="impl"
-            className={classes.textfield}
-            value={item.impl}
-            onChange={(e) => handleChange(e, index)}
-          />
-          <InputLabel className={classes.inputLabel}>Domains</InputLabel>
-          <InputLabel className={classes.inputLabel}>LAN</InputLabel>
-          <FormGroup row style={{ paddingBottom: "1rem" }}>
-            <TextField
-              className={classes.smallTextField}
-              size="small"
-              variant="outlined"
-              type="number"
-              label="Read"
-              value={item.domains.lan.read}
-            />
-            <TextField
-              className={classes.smallTextField}
-              size="small"
-              variant="outlined"
-              type="number"
-              label="Write"
-              value={item.domains.lan.write}
-            />
-            <TextField
-              className={classes.smallTextField}
-              size="small"
-              variant="outlined"
-              type="number"
-              label="Delete"
-              value={item.domains.lan.delete}
-            />
-          </FormGroup>
-          <InputLabel className={classes.inputLabel}>WAN</InputLabel>
-          <FormGroup row style={{ paddingBottom: "1rem" }}>
-            <TextField
-              className={classes.smallTextField}
-              size="small"
-              variant="outlined"
-              type="number"
-              label="Read"
-              value={item.domains.wan.read}
-            />
-            <TextField
-              className={classes.smallTextField}
-              size="small"
-              variant="outlined"
-              type="number"
-              label="Write"
-              value={item.domains.wan.write}
-            />
-            <TextField
-              className={classes.smallTextField}
-              size="small"
-              variant="outlined"
-              type="number"
-              label="Delete"
-              value={item.domains.wan.delete}
-            />
-            <TextField
-              className={classes.smallTextField}
-              size="small"
-              variant="outlined"
-              type="number"
-              label="Third Party Copy"
-              value={item.domains.wan.third_party_copy}
-            />
-          </FormGroup>
-          <Button
-            variant="contained"
-            color="primary"
-            disabled={index !== currentIndex}
-          >
-            Update Protocol
-          </Button>
-          <ColoredLine color="#000000" opacity={0.2} />
-        </div>
-      ))}
-    </TabPanel>
+        ))}
+      </TabPanel>
+      <ConfirmChangeDialog
+        open={open}
+        initialValues={initialProtocols}
+        currentIndex={currentIndex !== null ? currentIndex : 0}
+        changes={changes}
+        handleClose={() => setOpen(false)}
+      />
+    </React.Fragment>
   );
 }
 
